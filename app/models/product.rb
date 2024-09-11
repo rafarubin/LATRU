1e50aaca1d6c11a4e4996df988e7341ff6d6747a
Shinryo Okuhama <157136484+5h1nry0@users.noreply.github.com>
2024-09-11 13:15:04 -0500

    Agregar método en el modelo para consulta a OPENAI con imagenes en Cloudinary. Agregar lógica creación de producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,17 @@
+require "openai"
+require "json"
+
+
 class Product < ApplicationRecord
   has_many :historicals
   has_many_attached :photos
 
   def set_product_attributes
     Rails.cache.fetch("#{cache_key_with_version}/content") do
-      messages = [
-        { "type": "text", "text": "Necesito que analices ambas imagenes y me devuelvas únicamente un json con los siguientes keys. En primer lugar, los keys nombre, marca y cantidad_de_porciones y porciones_por_empaque con sus respectivos valores. Luego, los keys que empiecen con 'apto' con verdadero si el producto es apto para consumo según el criterio o falso si no es apto para el consumo. Además, quiero que en los keys que los demás keys los devuelvas su valor nutricional correspondiente.
+      version = "1/development"
 
+      messages = [
+        { "type": "text", "text": "Necesito que analices ambas imagenes y me devuelvas únicamente un json con los siguientes keys. En primer lugar, extrae de la imagen el nombre, marca y cantidad_de_porciones y porciones_por_empaque y colócalos en su respectiva key. Luego, los keys que empiecen con 'apto' con verdadero si el producto es apto para consumo según el criterio o falso si no es apto para el consumo. Además, quiero los demás keys los devuelvas su cantidad nutricional correspondiente.
         nombre:
         marca:
         cantidad:
@@ -31,22 +36,51 @@ class Product < ApplicationRecord
         "},
         { "type": "image_url",
             "image_url": {
-              "url": "https://www.ahorramas.com/dw/image/v2/BFNH_PRD/on/demandware.static/-/Sites-ahorramas-master/default/dw7c6310cc/Assets/026550_EIAh/large/a/c/c/8/acc8f7ce52b8cdc890323295ef6ee57b6e1edb92_026550_EIAh.jpg?sh=450",
+              "url": Cloudinary::Utils.cloudinary_url(photos[0].key, version: version),
+              "detail": "high"
           }
         },
         { "type": "image_url",
           "image_url": {
-            "url": "https://www.ahorramas.com/dw/image/v2/BFNH_PRD/on/demandware.static/-/Sites-ahorramas-master/default/dw7c6310cc/Assets/026550_EIAh/large/a/c/c/8/acc8f7ce52b8cdc890323295ef6ee57b6e1edb92_026550_EIAh.jpg?sh=450",
+            "url": Cloudinary::Utils.cloudinary_url(photos[1].key, version: version),
+            "detail": "high"
+        },
         }
-      }
       ]
+
       client = OpenAI::Client.new
-      chatgpt_response = client.chat(parameters: {
-        model: "gpt-4o",
-        response_format: { type: "json_object" },
-        messages: [{ role: "user", content: messages}]
-      })
-      chatgpt_response["choices"][0]["message"]["content"]
+
+      chat_response = client.chat(
+        parameters: {
+          model: "gpt-4o", # Required.
+          response_format: { "type": "json_object" },
+          messages: [{ role: "user", content: messages }] # Required.
+          })
+
+      hash = JSON.parse(chat_response["choices"][0]["message"]["content"])
+
+      self.update(
+      name: hash["nombre"],
+      brand: hash["marca"],
+      quantity: hash["cantidad"],
+      portion_qty: hash["porciones_por_empaque"],
+      gluten: hash["apto_gluten_free"],
+      dairy: hash["apto_intolertante_lactosa"],
+      penaut: hash["apto_alergia_mani"],
+      seafood: hash["apto_alergia_mariscos"],
+      soy: hash["apto_alergia_soya"],
+      egg: hash["apto_alergia_huevos"],
+      sesame: hash["apto_alergia_ajonjoli"],
+      sugar: hash["apto_cero_azucar"],
+      vegetarian: hash["apto_vegetariano"],
+      vegan: hash["apto_vegano"],
+      calories: hash["calorias"],
+      fat: hash["grasas"],
+      fat_trans: hash["grasas_trans"],
+      carb: hash["carbohidratos"],
+      protein: hash["proteinas"],
+      sugarqty: hash["azucar"]
+      )
     end
   end
 end
