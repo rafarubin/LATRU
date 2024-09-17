5b876d337940cf2c3b40bf42b86c67d41f7c3652
Paul Zapata <polzapata@outlook.es>
2024-09-17 10:56:57 -0500

    resultados-descripcion-fix

--- app/controllers/historicals_controller.rb
+++ app/controllers/historicals_controller.rb
@@ -1,14 +1,22 @@
 class HistoricalsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @profile = Profile.find(params[:profile_id])
-    @historicals = Historical.where(profile_id: @profile.id)
-  end
-
   def show
     @historical = Historical.find(params[:id])
     @product = @historical.product
     @user = User.find(params[:user_id])
     @profile = Profile.find(params[:profile_id])
+
+    # Lista de atributos relevantes
+    @attributes_to_display = []
+    attributes = %w[gluten peanut seafood soy egg sesame sugar vegetarian vegan dairy]
+
+    attributes.each do |attribute|
+      product_value = @product.send(attribute)
+      profile_value = @profile.send(attribute)
+
+      # Condición: verdadero en producto y falso en perfil
+      if product_value == true && profile_value == false
+        @attributes_to_display << attribute
+      end
+    end
   end
 end
