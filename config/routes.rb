476dbd86a2329e7b952f1e20b5e9bdff6cecd4cc
Shinryo Okuhama <157136484+5h1nry0@users.noreply.github.com>
2024-09-17 11:00:36 -0500

    cambiar textos en los formularios

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,7 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :profiles do
-      resources :products do
-        collection do
-          post "create_second_photo"
-        end
-      end
+      resources :products
       resources :historicals, only: [:index, :show, :destroy]
       resources :barcodes, only: [:new, :create, :show]
     end
